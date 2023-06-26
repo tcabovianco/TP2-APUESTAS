8728cf7ce060137130649ccbf417e66c10621bac
valeenmonsalvo <133068139+valeenmonsalvo@users.noreply.github.com>
2023-06-25 22:46:36 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -557,6 +557,39 @@ def apostar(equipoId: int, idUser: int, datosTotales: list, montoDisponible: flo
     os.remove(f"fixture_{elegirId}.json")
     os.remove("fixtures.json")
 
+def obtener_usuario_mas_ganador() -> None:
+    # Obtiene usuario más ganador a partir del archivo transacciones.
+    victorias_por_usuario = {}
+
+    with open("transacciones.csv", "r") as fT:
+        for linea in fT:
+            datos = linea.strip().split(",")
+            idUser = int(datos[0])
+            resultadoFinal = datos[2]
+
+            if resultadoFinal == "Gana":
+                if idUser in victorias_por_usuario:
+                    victorias_por_usuario[idUser] += 1
+                else:
+                    victorias_por_usuario[idUser] = 1
+
+    
+    if not victorias_por_usuario:
+        print("No se encontraron usuarios que hayan apostado.")
+        return
+
+   
+    if not any(resultado == "Gana" for resultado in victorias_por_usuario.values()):
+        print("No hubo ganadores entre los usuarios.")
+        return
+
+    usuario_mas_ganador = max(victorias_por_usuario, key=victorias_por_usuario.get)
+    cantidad_victorias = victorias_por_usuario[usuario_mas_ganador]
+
+    print(f"El usuario más ganador es: {usuario_mas_ganador}")
+    print(f"Cantidad de victorias: {cantidad_victorias}")
+
+
 def main() -> None:
     #Sección principal del código
 
